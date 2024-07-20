0a75fc97d45b7239e6a17a1d4dc6faae9c1706c2
Kiran Raj <kiranrajk2003@gmail.com>
2024-07-20 12:22:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => (
+  <>
+    <main className="relative">
+          Nav
+          <section className="xl:padding-1">
+              Hero
+          </section>
+    </main>
+  </>
+);
+
+export default App;
